31647a3658963bba820db1fd5bca5550d85fcc82
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2023-06-20 10:44:01 +0200

    created classV2: validation annotations for offset and limit

---
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/custom/validationsC/ValidLimit.java
@@ -0,0 +1,26 @@
+package com.itachallenge.challenge.custom.validationsC;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import jakarta.validation.ReportAsSingleViolation;
+import jakarta.validation.constraints.Min;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Min(value = 1) //doesn't have sense limit the page size to less than one element
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = {})
+@ReportAsSingleViolation
+//https://docs.jboss.org/hibernate/stable/validator/reference/en-US/html_single/#section-constraint-composition
+public @interface ValidLimit {
+
+    String message() default "{limit.error}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
