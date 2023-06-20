b948676ece25a5999756b9f4b2c14a842ec8aaea
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2023-06-20 12:19:53 +0200

    TODO class: @Service implementation

---
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/custom_v2/services_v2/ChallengesServiceDefaultTODO.java
@@ -0,0 +1,15 @@
+package com.itachallenge.challenge.custom_v2.services_v2;
+
+import com.itachallenge.challenge.custom_v2.dtos_v2.PageDtoTODO;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class ChallengesServiceDefaultTODO implements ChallengesServiceTODO{
+
+    //TODO: implement method
+    @Override
+    public Mono<PageDtoTODO> findChallengesPage(int offset, int limit) {
+        return null;
+    }
+}
