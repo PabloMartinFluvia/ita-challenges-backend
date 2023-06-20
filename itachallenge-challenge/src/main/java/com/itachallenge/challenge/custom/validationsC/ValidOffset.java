c0cc3afe09f6d62f6f1ffa0da6de4ee976dae359
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2023-06-20 10:44:01 +0200

    NEW class: validation annotations for offset and limit

---
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/custom/validationsC/ValidOffset.java
@@ -0,0 +1,26 @@
+package com.itachallenge.challenge.custom.validationsC;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import jakarta.validation.ReportAsSingleViolation;
+import jakarta.validation.constraints.Min;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Min(value = 0) //doesn't have sense exclude a negative value
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = {})
+@ReportAsSingleViolation
+//https://docs.jboss.org/hibernate/stable/validator/reference/en-US/html_single/#section-constraint-composition
+public @interface ValidOffset {
+
+    String message() default "{offset.error}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
